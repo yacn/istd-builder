5f79006b6157daf3ce7d2784f8569d4795a4f0ec
lastlink <theefunk@gmail.com>
2018-09-17 00:26:55 -0400

    mae some preogresso

--- nodejs/scraper.js
+++ nodejs/scraper.js
@@ -5,7 +5,7 @@ const MAINCONFIG = require('./config/mainconfig.js');
 async function run() {
   const browser = await puppeteer.launch({
     headless: MAINCONFIG.headless,
-    args: ["--ash-host-window-bounds=1920x1080", "--window-size=1920,1048", "--window-position=0,0"],
+    args: ["--ash-host-window-bounds=1920x1080", "--window-size=1920,1048", "--window-position=0,0"]
   });
 
   const page = await browser.newPage();
@@ -45,9 +45,83 @@ async function run() {
   await page.waitForNavigation();
 
   await page.goto(MAINCONFIG.urlList.courses);
+  await page.waitForNavigation();
+
   await page.waitFor(2 * 1000);
   console.log("finished logging in")
+  // get courses list urls
+  // MAINCONFIG.courseList.coursesSelector
+
+  // var courseList = []
+  // insert into course list as
+  // {
+  //   name: "",
+  //   href: "",
+  //   assignments: []
+  // }
+  // var data = document.querySelectorAll(MAINCONFIG.courseList.coursesSelector);
+  await page.waitFor(MAINCONFIG.courseList.coursesSelector);
+  var courseList = await page.evaluate((courseListConfig) => {
+    var tmpList = [];
+    // return courseListConfig.coursesSelector;
+    const courses = Array.from(document.querySelectorAll(courseListConfig.coursesSelector));
+    // return courses.length;
+    return courses[0].innerHTML;
+    tmpList.push({
+      name: "",
+      href: "",
+      assignments: []
+    })
+    // for (let i = 0; i < courses.length; i++) {
+    var tmpRow = ""
+    tmpRow = courses.forEach(row => {
+      return row.innerHTML;
+      // return row.innerHTML;
+      try {
+        var cText = row.querySelector(courseListConfig.courseNameSelector);
+        // console.log(cText)
+        course.name = cText;
+
+        var course = {
+
+          assignments: []
+        }
+        // const row = data[i];
+        // console.log(element.innerText)
+        // console.log(i)
+        // console.log(row.querySelectorAll("div"))
+        var href = ""
+        if (courseListConfig.courseHrefFunction) {
+          href = eval('row' + courseListConfig.courseHrefFunction);
+
+          // console.log(href.substr(MAINCONFIG.courseList.hrefStartString.length, href.indexOf(MAINCONFIG.courseList.hrefEndString) - MAINCONFIG.courseList.hrefStartString.length))
+
+        }
+        // var href = element.querySelector("div > div:nth-child(2)").getAttribute('onclick')
+        // console.log(href)
+        course.href = href;
+        // data[0].querySelector("div > div:nth-child(2)").getAttribute('onclick')
+
+        return course;
+        // console.log(element.querySelector("div:nth-child(2) > div:nth-child(1)").innerText)
+        tmpList.push(course)
+      }
+      catch (error) {
+        console.log("failed to retrieve input value")
+
+        console.log(error)
+        return error;
+      }
+    })
+    return tmpRow
+    return tmpList;
+  }, MAINCONFIG.courseList)
+  // iterate over each course
+  console.log("finished course parsing")
+  console.log(courseList);
   exit();
+
+  // export final results to input/assignments_template
 
   if (MAINCONFIG.closeBrowser)
     browser.close();
