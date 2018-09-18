f9f33a691503c5986295e07924f523d5664cb867
lastlink <theefunk@gmail.com>
2018-09-18 18:44:03 -0400

    working iteration

--- nodejs/scraper.js
+++ nodejs/scraper.js
@@ -149,191 +149,200 @@ async function run() {
     // div.d2l-collapsepane-content > div > div> div> div.d2l-datalist-container.d2l-datalist-style1 > ul.d2l-datalist.vui-list > li.d2l-datalist-item.d2l-datalist-simpleitem
     if (MAINCONFIG.course.tobCSelector) {
       // generate assignments list
-      var assign = document.querySelectorAll("div.d2l-collapsepane-content > div > div> div> div.d2l-datalist-container.d2l-datalist-style1 > ul.d2l-datalist.vui-list > li.d2l-datalist-item.d2l-datalist-simpleitem")
+      var assignmentHeader = ["priority", "due_date", "notes", "course_name", "notify", "name"]
+      await page.waitFor(MAINCONFIG.course.assignmentSelector);
+      var assignmentList = await page.evaluate((courseConfig) => {
 
-      var priorities = [
-        {
-          keywords: ['exam', "report", "project"],
-          priority: 3
-        },
-        {
-          keywords: ['quiz', "due"],
-          priority: 2
-        }
-      ]
-      var defaultPriority = 2;
-      var removeWords = ["I'm Done"]
-      var cutOff = "Due"
-      var currentDate = new Date;
-      var lastDate;
-      var firstDate = new Date(1970, 01, 12);
-      var oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+        var assignList = []
+        var assign = document.querySelectorAll("div.d2l-collapsepane-content > div > div> div> div.d2l-datalist-container.d2l-datalist-style1 > ul.d2l-datalist.vui-list > li.d2l-datalist-item.d2l-datalist-simpleitem")
 
-      var assignmentHeader = ["priority", "due_date", "notes", "course_uid", "notify	total_points", "name"]
-      var assignmentList = []
-      for (let k = 0; k < assign.length; k++) {
-        var assignment = []
-        var diffDays=""
-        var notes = ""
-        var notification_time = "";
+        var priorities = [
+          {
+            keywords: ['exam', "report", "project"],
+            priority: 3
+          },
+          {
+            keywords: ['quiz', "due"],
+            priority: 2
+          }
+        ]
+        var defaultPriority = 2;
+        var removeWords = ["I'm Done"]
+        var cutOff = "Due"
+        var currentDate = new Date;
+        var lastDate;
+        var firstDate = new Date(1970, 01, 12);
+        var oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
 
 
-        const element = assign[k];
-        // console.log(element.innerText);
-        var fulltext = element.innerText
-        removeWords.forEach(word => {
-          fulltext = fulltext.replace(word, "")
+        for (let k = 0; k < assign.length; k++) {
+          var assignment = []
+          var diffDays = ""
+          var notes = ""
+          var notification_time = "";
 
-        });
-        // var indexOfStevie = myArray.findIndex(i => i.hello === "stevie");
 
-        var priority = defaultPriority;
-        var priorityObj = -1;
+          const element = assign[k];
+          console.log(element.innerText);
+          var fulltext = element.innerText
+          removeWords.forEach(word => {
+            fulltext = fulltext.replace(word, "")
 
-        for (let p = 0; p < priorities.length; p++) {
-          for (let k = 0; k < priorities[p].keywords.length; k++) {
-            const keyword = priorities[p].keywords[k];
-            if (fulltext.indexOf(keyword) != -1) {
+          });
+          // var indexOfStevie = myArray.findIndex(i => i.hello === "stevie");
 
-              priority = priorities[p].priority
+          var priority = defaultPriority;
+          var priorityObj = -1;
+
+          for (let p = 0; p < priorities.length; p++) {
+            for (let k = 0; k < priorities[p].keywords.length; k++) {
+              const keyword = priorities[p].keywords[k];
+              if (fulltext.indexOf(keyword) != -1) {
+
+                priority = priorities[p].priority
+                break;
+              }
+            }
+            if (priorityObj != -1)
               break;
-            }
-          }
-          if (priorityObj != -1)
-            break;
-
-        }
-        console.log(priorityObj)
-        // var indexOfPriority = priorities.findIndex(i => { 
-        //   i.keywords.indexOf(fulltext)
-        // );
-        console.log(fulltext)
-        var finalTxt = fulltext;
-        var due_date;
-        var notification;
-        var monthNames = ["January", "February", "March", "April", "May", "June",
-          "July", "August", "September", "October", "November", "December"];
-        if (fulltext.indexOf(cutOff) != -1) {
-          finalTxt = fulltext.substr(0, fulltext.indexOf(cutOff))
-          notes += fulltext.substr(fulltext.indexOf(cutOff), fulltext.length)
-
-          // retrieve date
-          var monthIndex = -1;
-          var monthNum = -1;
-          var monthName = ""
-          // monthIndex=monthNames.indexOf()
-          for (let j = 0; j < monthNames.length; j++) {
-            const element = monthNames[j];
-            monthIndex = notes.indexOf(element)
-            if (monthIndex != -1) {
-              monthNum = monthNames.indexOf(element)
-              monthName = element;
-              break
-            }
 
           }
-          // console.log(monthIndex)
-          console.log(monthNum)
+          // console.log(priorityObj)
+          // var indexOfPriority = priorities.findIndex(i => { 
+          //   i.keywords.indexOf(fulltext)
+          // );
+          // console.log(fulltext)
+          var finalTxt = fulltext;
+          var due_date;
+          var notification;
+          var monthNames = ["January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"];
+          if (fulltext.indexOf(cutOff) != -1) {
+            finalTxt = fulltext.substr(0, fulltext.indexOf(cutOff))
+            notes += fulltext.substr(fulltext.indexOf(cutOff), fulltext.length)
 
-          console.log(monthName)
+            // retrieve date
+            var monthIndex = -1;
+            var monthNum = -1;
+            var monthName = ""
+            // monthIndex=monthNames.indexOf()
+            for (let j = 0; j < monthNames.length; j++) {
+              const element = monthNames[j];
+              monthIndex = notes.indexOf(element)
+              if (monthIndex != -1) {
+                monthNum = monthNames.indexOf(element)
+                monthName = element;
+                break
+              }
 
-          var monthDay = -1;
-          var hours = 0;
-          var minutes = 0;
-          if (monthIndex != -1 && notes.indexOf("at") != -1) {
-            // console.log(fulltext)
-            // console.log(monthName.length + monthIndex)
-            // console.log(fulltext.indexOf(monthName))
-
+            }
             // console.log(monthIndex)
-            // console.log(fulltext.indexOf("at"))
-            // console.log(fulltext.indexOf("at") - (monthName.length + monthIndex))
-            monthDay = notes.substr(monthName.length + monthIndex, notes.indexOf("at") - (monthName.length + monthIndex));
-            // console.log(parseInt(atIndex))
+            // console.log(monthNum)
 
-            // retrieve time
-            var hourIndex = notes.indexOf("at") + "at".length
-            var minutIndex = notes.indexOf(":") + ":".length
+            // console.log(monthName)
 
-            var prefixIndex = notes.indexOf("PM")
-            var hourOffset = 0;
-            if (prefixIndex != -1) {
-              hourOffset = 12
-            } else
-              prefixIndex = notes.indexOf("AM")
+            var monthDay = -1;
+            var hours = 0;
+            var minutes = 0;
+            if (monthIndex != -1 && notes.indexOf("at") != -1) {
+              // console.log(fulltext)
+              // console.log(monthName.length + monthIndex)
+              // console.log(fulltext.indexOf(monthName))
 
-            console.log(hourIndex + ":" + minutIndex + ":" + prefixIndex)
+              // console.log(monthIndex)
+              // console.log(fulltext.indexOf("at"))
+              // console.log(fulltext.indexOf("at") - (monthName.length + monthIndex))
+              monthDay = notes.substr(monthName.length + monthIndex, notes.indexOf("at") - (monthName.length + monthIndex));
+              // console.log(parseInt(atIndex))
+
+              // retrieve time
+              var hourIndex = notes.indexOf("at") + "at".length
+              var minutIndex = notes.indexOf(":") + ":".length
+
+              var prefixIndex = notes.indexOf("PM")
+              var hourOffset = 0;
+              if (prefixIndex != -1) {
+                hourOffset = 12
+              } else
+                prefixIndex = notes.indexOf("AM")
+
+              // console.log(hourIndex + ":" + minutIndex + ":" + prefixIndex)
 
 
-            if (hourIndex != -1 & minutIndex != -1 & prefixIndex != -1) {
-              // console.log( minutIndex - (fulltext.length - minutIndex))
-              hours = notes.substr(hourIndex, minutIndex - hourIndex - 1)
-              hours = isNaN(parseInt(hours)) ? 0 : parseInt(hours) + hourOffset
-              console.log("hr:" + hours)
-              minutes = notes.substr(minutIndex, prefixIndex - minutIndex - 1)
-              minutes = isNaN(parseInt(minutes)) ? 0 : parseInt(minutes)
+              if (hourIndex != -1 & minutIndex != -1 & prefixIndex != -1) {
+                // console.log( minutIndex - (fulltext.length - minutIndex))
+                hours = notes.substr(hourIndex, minutIndex - hourIndex - 1)
+                hours = isNaN(parseInt(hours)) ? 0 : parseInt(hours) + hourOffset
+                // console.log("hr:" + hours)
+                minutes = notes.substr(minutIndex, prefixIndex - minutIndex - 1)
+                minutes = isNaN(parseInt(minutes)) ? 0 : parseInt(minutes)
 
-              // console.log( prefixIndex - (fulltext.length - prefixIndex))
+                // console.log( prefixIndex - (fulltext.length - prefixIndex))
 
-              console.log("minutes:" + minutes)
+                // console.log("minutes:" + minutes)
+              }
+
+
+
             }
+            // console.log("monthday:" + monthDay)
 
 
+            // if(monthIndex!=-1)
+            // monthIndex++;
+            var secondDate = new Date();
+            if (monthDay != -1 && monthNum != -1) {
+              secondDate = new Date(courseConfig.defaultYear || currentDate.getFullYear(), monthNum, monthDay, hours, minutes);
+            }
+            // var date  = new Date(2018,11,3,05,30)
+
+
+            // console.log(secondDate)
+
+            diffDays = Math.round(Math.abs((firstDate.getTime() - secondDate.getTime()) / (oneDay)));
+            // var d = new Date(year, month, day, hours, minutes, seconds, milliseconds);
+
+            // console.log(diffDays)
+            if (hours != 0 || minutes != 0)
+              notification_time = secondDate.getHours() * 60 * 60 + secondDate.getMinutes() * 60
+            // console.log(notification_time)
+            // console.log("
+            // days":)
+
+
+            // var finalTxt = fulltext.indexOf(cutOff) == -1 ? fulltext : fulltext.substr(0, fulltext.indexOf(cutOff))
+            // console.log("finalTxt:" + finalTxt)
+
+            try {
+              var href = element.querySelector("div > div > div > div > a").href
+              console.log(href)
+              notes += "\n" + href
+            } catch (error) {
+              // console.log(error)
+            }
+            // console.log("Notes:" + notes)
+            if (k == 25)
+              break;
+          } else {
+            // may be a reading a ssignment set priorty to 1
+            priority = 1;
 
           }
-          console.log("monthday:" + monthDay)
+          // console.log("prioty" + priority)
+          assignment.push(priority);
+          assignment.push(diffDays);
+          assignment.push(notes)
+          // assignment.push(course.name)
+          assignment.push(notification_time)
+          assignment.push(finalTxt)
+          console.log(assignment)
+          assignList.push(assignment)
+        } // end assignments 4 loop
+        return assignList;
+      }, MAINCONFIG.course);
 
-
-          // if(monthIndex!=-1)
-          // monthIndex++;
-          var secondDate = new Date();
-          if (monthDay != -1 && monthNum != -1) {
-            secondDate = new Date(MAINCONFIG.course.defaultYear || currentDate.getFullYear(), monthNum, monthDay, hours, minutes);
-          }
-          // var date  = new Date(2018,11,3,05,30)
-
-
-          console.log(secondDate)
-
-          diffDays = Math.round(Math.abs((firstDate.getTime() - secondDate.getTime()) / (oneDay)));
-          // var d = new Date(year, month, day, hours, minutes, seconds, milliseconds);
-
-          console.log(diffDays)
-          if (hours != 0 || minutes != 0)
-            notification_time = secondDate.getHours() * 60 * 60 + secondDate.getMinutes() * 60
-          console.log(notification_time)
-          // console.log("
-          // days":)
-
-
-          // var finalTxt = fulltext.indexOf(cutOff) == -1 ? fulltext : fulltext.substr(0, fulltext.indexOf(cutOff))
-          console.log("finalTxt:" + finalTxt)
-
-          try {
-            var href = element.querySelector("div > div > div > div > a").href
-            console.log(href)
-            notes += "\n" + href
-          } catch (error) {
-            // console.log(error)
-          }
-          console.log("Notes:" + notes)
-          if (k == 25)
-            break;
-        } else {
-          // may be a reading a ssignment set priorty to 1
-          priority = 1;
-
-        }
-        console.log("prioty" + priority)
-        assignment.push(priority);
-        assignment.push(diffDays);
-        assignment.push(notes)
-        // assignment.push(course.name)
-        assignment.push(notification_time)
-        assignment.push(finalTxt)
-        console.log(assignment)
-      } // end assignments 4 loop
-
+      console.log("assignment list")
+      console.log(assignmentList)
       // will need to go to table of contents, 
       // get all assignments, assign priority, gives notes and due date
       // then by name find quizzes & assignments and get direct href
